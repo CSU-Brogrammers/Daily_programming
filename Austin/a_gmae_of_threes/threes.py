090153a54eee168df9123c3f71a04db47a6f6a28
Austin Smitherman <asmitherman@mail.csuchico.edu>
2017-02-07 23:10:19 -0800

    Austin's implementation of A game of threes in python

---
+++ Austin/a_gmae_of_threes/threes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+def threes(num):
+    while num != 1:
+        print(num)
+        if num % 3 == 0:
+            num = num/3
+        elif (num - 1) % 3 == 0:
+            num -= 1
+            num = num/3
+        elif (num + 1) % 3 == 0:
+            num += 1
+            num = num/3
+
+
+threes(31337357)
+# threes(5)
+# threes(4)
